3232b0719c9d1e89d7ec476d5d5664b6f1c72458
Andrey Margashov <Andrey.Margashov@yandex.ru>
2018-09-24 20:51:46 +0300

    Class Router continue development

--- core/base/Router.php
+++ core/base/Router.php
@@ -40,20 +40,48 @@ class Router {
         $path_base = Application::getConfig()->routes->default->base;
         $path_dyn = Application::getConfig()->routes->default->dynamic;
 
-        // добавить маршруты приложения
-        $this->add($path_base->regexp, [$path_base->controller, $path_base->action]);
-        $this->add($path_dyn->regexp);
-        // добавить маршруты админки
+        // добавить маршруты админки 
+        // (перед маршрутами приложения по умолчанию, так как регулярка более специфичная)
         $admin_uri = Application::getConfig()->admin->uri;
-        $this->add(substr_replace($path_base->regexp, $admin_uri , 1, 0), [$path_base->controller, $path_base->action]);
+        $this->add(substr_replace($path_base->regexp, $admin_uri , 1, 0), ["controller" => $path_base->controller, "action" => $path_base->action]);
         $this->add(substr_replace($path_dyn->regexp, $admin_uri . '/', 1, 0));
+        // добавить маршруты приложения
+        $this->add($path_base->regexp, ["controller" => $path_base->controller, "action" => $path_base->action]);
+        $this->add($path_dyn->regexp);
     }
+    
     
     public function dispatch($uri) {
-        $this->matchRoute($uri);
+        if ($this->matchRoute($uri)) {
+            echo 'Match found !!!!';
+        } else {
+            echo 'Match not found (((';
+        }
     }
     
+    /**
+     * Формируем маршрут [controller, action] на основе совпадения uri с шаблоном регулярки
+     * @param type $uri
+     * @return boolean true, если совпадение в таблице маршрутов найдено
+     */
     private function matchRoute($uri) {
-       
+        foreach ($this->routes as $pattern => $route) {
+            $matches = [];
+            if (preg_match("#{$pattern}#", $uri, $matches)) {
+                // цикл ниже сработает только для uri типа "/controller/action" (или только "/controller")
+                foreach ($matches as $key => $value) {
+                     if (is_string($key)) {
+                         $route[$key] = $value;
+                     }
+                }
+                // цикл ниже сработает только для uri типа "/controller"
+                if (!array_key_exists('action', $route)) {
+                    $route['action'] = Application::getConfig()->routes->default->dynamic->action;
+                }
+                $this->route = $route;
+                return true;
+            }
+        }
+        return false;
     }
 }
